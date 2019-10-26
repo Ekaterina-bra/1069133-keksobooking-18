0a936894e4a2c42261f702ed9b52690218e863ad
EkaterinaBraun <55137912+Ekaterina-bra@users.noreply.github.com>
2019-10-26 16:49:56 +0300

    Отображает карточку первого обьявления

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@ var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
 var generateAdd = function () {
   var x = getRandomInt(X_MIN, X_MAX);
   var y = getRandomInt(Y_MIN, Y_MAX);
@@ -85,3 +84,47 @@ for (var i = 0; i < ADDS_NUMBER; i++) {
 }
 pins.appendChild(fragment);
 
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card popup');
+
+var renderCard = function (pinData) {
+  var pinDataElement = cardTemplate.cloneNode(true);
+  pinDataElement.querySelector('.popup__title').textContent = pinData.offer.title;
+  pinDataElement.querySelector('.popup__text--address').textContent = pinData.offer.address;
+  pinDataElement.querySelector('.popup__text--price').textContent = pinData.offer.price + '₽/ночь';
+  var typeName = '';
+  switch (pinData.offer.type) {
+    case 'flat':
+      typeName = 'Квартира';
+      break;
+    case 'bungalo':
+      typeName = 'Бунгало';
+      break;
+    case 'house':
+      typeName = 'Дом';
+      break;
+    case 'palace':
+      typeName = ' Дворец';
+      break;
+  }
+  pinDataElement.querySelector('.popup__type').textContent = typeName;
+  pinDataElement.querySelector('.popup__text--capacity').textContent = pinData.offer.rooms + ' комнаты для' + pinData.offer.guests + ' гостей';
+  pinDataElement.querySelector('.popup__text--time').textContent = 'Заезд после' + pinData.offer.checkin + ', выезд до' + pinData.offer.checkout;
+  pinDataElement.querySelector('.popup__features').textContent = pinData.offer.features;
+  pinDataElement.querySelector('.popup__description').textContent = pinData.offer.description;
+  var photosBlock = pinDataElement.querySelector('.popup__photos');
+  var photosList = pinData.offer.photos;
+  for (var j = 0; j < photosList.length; j++) {
+    var photoElement = photosBlock.querySelector('img').cloneNode(true);
+    photoElement.src = photosList[j];
+    photosBlock.appendChild(photoElement);
+  }
+  var avatarBlock = pinDataElement.querySelector('.popup__avatar');
+  var avatarElement = avatarBlock.querySelector('img');
+  avatarElement.src = pinData.author.avatar;
+  return pinDataElement;
+};
+renderCard(newPins[0]);
+
+var filtersContainer = document.querySelector('.map__filters-container');
+userDialog.appendChild(renderCard(newPins[0]));
+filtersContainer.insertBefore(userDialog);
